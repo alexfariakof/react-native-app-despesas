7a2d433ad1a63af5313ef675695e2369e3c31ce0
Alex Ribeiro <alexfariakof@gmail.com>
2019-04-12 11:30:37 -0300

    Implementação classe Usuario mock

---
+++ BackEnd/apiDespesasPessoais/Model/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiDespesasPessoais.Model
+{
+    public class Usuario
+    {
+        public long idUsuario { get; set; }
+        public String nome { get; set; }
+        public String email { get; set; }
+        public String senha { get; set; }
+    }
+}
