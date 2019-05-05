85b1197daa4aab229bad95119411db844b999298
Alex Ribeiro <alexfariakof@gmail.com>
2019-05-05 12:46:10 -0300

    Adicionando scroliing na tela de Login

--- FrontEnd/src/screens/HomeScreen/index.js
+++ FrontEnd/src/screens/HomeScreen/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, ImageBackground, TextInput, Image, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { View, Text, ImageBackground, TextInput, Image, TouchableOpacity, ActivityIndicator, ScrollView } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage'
 
 import assets from './assets'
@@ -20,13 +20,13 @@ class HomeScreen extends Component {
             senha: null
         }
     }
-    
+
     async componentDidMount() {
         await AsyncStorage.clear();
         this.setState({ isLoading: false })
     }
 
-    signIn = async () => {        
+    signIn = async () => {
         if (!this.isValid(this.state))
             return;
 
@@ -48,7 +48,7 @@ class HomeScreen extends Component {
                 else
                     this.setState({ erroMessage: json.message });
                 this.setState({ isLoading: false });
-            });           
+            });
         }
         catch (error) {
             this.setState({ isLoading: false });
@@ -66,7 +66,7 @@ class HomeScreen extends Component {
         }
         else
             body.errors.login = null;
-    
+
         if ((body.senha === null) || (body.senha === undefined) || (body.senha.trim() === '')) {
             body.errors.senha = 'Senha não pode ser nula!';
             result = false;
@@ -76,7 +76,7 @@ class HomeScreen extends Component {
 
         this.setState({ errors: body.errors });
         return result;
-    }    
+    }
 
     render() {
         const { isLoading } = this.state;
@@ -87,49 +87,51 @@ class HomeScreen extends Component {
                 imageStyle={{ resizeMode: 'stretch' }}
                 style={styles.background}
             >
-                <View style={styles.body}>
-                    <View style={styles.ViewCentralizar} >
-                        <Text style={styles.textBenVindo} >Seja Bem Vindo</Text>
-
-                        <TouchableOpacity onPress={() => this.props.navigation.navigate('Cadastro')} >
-                            <Text style={styles.btnCadastrar}>Cadastre-se</Text>
-                        </TouchableOpacity>
-                    </View>
-                    <View>
-                        <TextInput style={styles.text} placeholder='Digite seu email' maxLength={20} keyboardType='email-address' onChangeText={txt => this.setState({ login: txt })} />
+                <ScrollView>
+                    <View style={styles.body}>
                         <View style={styles.ViewCentralizar} >
-                            <Text style={{ color: 'red' }}> {this.state.errors.login} </Text>
-                        </View>
+                            <Text style={styles.textBenVindo} >Seja Bem Vindo</Text>
 
-                        <TextInput style={styles.text} placeholder='Digite sua senha' maxLength={8} secureTextEntry onChangeText={txt => this.setState({ senha: txt })} />
+                            <TouchableOpacity onPress={() => this.props.navigation.navigate('Cadastro')} >
+                                <Text style={styles.btnCadastrar}>Cadastre-se</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View>
+                            <TextInput style={styles.text} placeholder='Digite seu email' maxLength={20} keyboardType='email-address' onChangeText={txt => this.setState({ login: txt })} />
+                            <View style={styles.ViewCentralizar} >
+                                <Text style={{ color: 'red' }}> {this.state.errors.login} </Text>
+                            </View>
+
+                            <TextInput style={styles.text} placeholder='Digite sua senha' maxLength={8} secureTextEntry onChangeText={txt => this.setState({ senha: txt })} />
+                            <View style={styles.ViewCentralizar} >
+                                <Text style={{ color: 'red' }}> {this.state.errors.senha} </Text>
+                            </View>
+                        </View>
                         <View style={styles.ViewCentralizar} >
-                            <Text style={{ color: 'red' }}> {this.state.errors.senha} </Text>
+                            <Text style={{ color: 'red' }}> {this.state.erroMessage} </Text>
+                        </View>
+                        <View style={styles.ViewCentralizar} >
+                            <TouchableOpacity onPress={() => this.props.navigation.navigate('RecuperarSenha')} >
+                                <Text style={styles.btnEsqueciSenha}>Esqueci minha senha</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.ViewCentralizar} >
+                            {isLoading ? (
+                                <ActivityIndicator
+                                    style={styles.btnIniciar}
+                                    color="#C00"
+                                    size="large"
+                                />
+                            ) :
+                                (
+                                    <TouchableOpacity style={styles.btnIniciar} onPress={() => { this.signIn() }}>
+                                        <Image source={assets.btnIniciar} />
+                                    </TouchableOpacity>
+                                )
+                            }
                         </View>
                     </View>
-                    <View style={styles.ViewCentralizar} >
-                        <Text style={{ color: 'red' }}> {this.state.erroMessage} </Text>
-                    </View>
-                    <View style={styles.ViewCentralizar} >
-                        <TouchableOpacity onPress={() => this.props.navigation.navigate('RecuperarSenha')} >
-                            <Text style={styles.btnEsqueciSenha}>Esqueci minha senha</Text>
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.ViewCentralizar} >
-                        {isLoading ? (
-                            <ActivityIndicator
-                                style={styles.btnIniciar}
-                                color="#C00"
-                                size="large"
-                            />
-                        ) :
-                            (
-                                <TouchableOpacity style={styles.btnIniciar} onPress={() => { this.signIn() }}>
-                                    <Image source={assets.btnIniciar} />
-                                </TouchableOpacity>
-                            )
-                        }
-                    </View>
-                </View>
+                </ScrollView>
             </ImageBackground>
         );
     }
